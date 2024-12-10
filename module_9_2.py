f2395a5eec6060e63da6527c89c483949e63b215
IlonaWishec <ilonawishec.management@gmail.com>
2024-12-10 21:07:43 +0300

    module_9_2.py

--- module_9_2.py
+++ module_9_2.py
@@ -5,8 +5,9 @@ second_result = [(x, y) for x in first_strings for y in second_strings if len(x)
 third_strings = first_strings + second_strings
 third_result = {x: len(x) for x in third_strings if len(x) % 2 == 0}
 
-
-
 print(first_result)
 print(second_result)
 print(third_result)
+# [10, 8, 8]
+# [('Elon', 'Task'), ('Elon', 'Java'), ('Musk', 'Task'), ('Musk', 'Java'), ('Monitors', 'Computer'), ('Variable', 'Computer')]
+# {'Elon': 4, 'Musk': 4, 'Programmer': 10, 'Monitors': 8, 'Variable': 8, 'Task': 4, 'Java': 4, 'Computer': 8}
